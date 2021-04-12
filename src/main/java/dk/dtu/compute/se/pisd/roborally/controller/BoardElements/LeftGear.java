75f10d7df173f399b933f3b7fac62855e3c60b48
UnknownBrain <gufa1353@gmail.com>
2021-04-12 16:00:00 +0200

    right and left turn gears added

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/BoardElements/LeftGear.java
@@ -0,0 +1,15 @@
+package dk.dtu.compute.se.pisd.roborally.controller.BoardElements;
+
+import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class LeftGear extends FieldAction {
+
+
+    @Override
+    public boolean doAction(GameController gameController, Space space) {
+        gameController.turnLeft(space.getPlayer());
+        return false;
+    }
+}
